0df858b72e39c0f31499d6b26a30fa5c8959025b
pitamberrana <pitamber11rana@gmail.com>
2022-09-09 14:23:35 +0545

    ex.13.8

--- blog-app/controllers/blogs.js
+++ blog-app/controllers/blogs.js
@@ -1,13 +1,35 @@
 const router = require("express").Router();
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
+const { SECRET } = require("../utils/config");
+const jwt = require("jsonwebtoken");
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
   next();
 };
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
+
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
   console.log(
     JSON.stringify(
       blogs.map((n) => `${n.author}: '${n.title}', ${n.likes} likes`),
@@ -26,32 +48,37 @@ router.get("/:id", blogFinder, async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", tokenExtractor, async (req, res) => {
   try {
-    const blog = await Blog.create(req.body);
+    const blog = await Blog.create({
+      ...req.body,
+      userId: req.decodedToken.id,
+    });
     return res.json(blog);
   } catch (error) {
     return res.status(400).json({ error });
   }
 });
 
-router.delete("/:id", blogFinder, async (request, response) => {
+router.delete("/:id", blogFinder, async (req, res) => {
   if (req.blog) {
     await req.blog.destroy();
   }
   res.status(204).end();
 });
-//   await Blog.destroy({
-//     where: {
-//       author: blog.author,
-//     },
-//   });
-//   response.status(200).json({ message: "blog deleted" });
-// });
 
 router.put("/:id", blogFinder, async (req, res) => {
   if (req.blog) {
     (req.blog.likes = req.body.likes), await req.blog.save();
+    res.json(req.blog);
+  } else {
+    res.status(204).end();
+  }
+});
+
+router.put("/:username", blogFinder, async (req, res) => {
+  if (req.blog) {
+    (req.blog.username = req.body.username), await req.blog.save();
     res.json(req.blog);
   } else {
     res.status(204).end();
